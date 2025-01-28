c80eb1ce2ba3ed164df2a264ff2325986e3a99ba
devfemibadmus <devfemibadmus@gmail.com>
2025-01-28 02:10:00 +0100

    🚑️ depl 🔒 oyed 📄

--- company/urls.py
+++ company/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('cars', views.cars, name='cars'),
     path('developer', views.developer, name='developer'),
+    re_path(r'^.*$', views.home, name="Home"),
 ]
