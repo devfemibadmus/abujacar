05cf53cf55e4030376c9072e35dd3c6d72904a9d
devfemibadmus <devfemibadmus@gmail.com>
2025-01-28 02:09:53 +0100

    🍻 producti 🛠 on files 🔖

---
+++ passenger_wsgi.py
@@ -0,0 +1,11 @@
+import importlib.util
+import os
+import sys
+
+sys.path.insert(0, os.path.dirname(__file__))
+
+wsgi_spec = importlib.util.spec_from_file_location('wsgi', 'abujacar/wsgi.py')
+wsgi = importlib.util.module_from_spec(wsgi_spec)
+wsgi_spec.loader.exec_module(wsgi)
+
+application = wsgi.application
